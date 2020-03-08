46850f57e84322c268cf307f732d158cc267acf2
pw_ydsoft <pw_ydsoft@126.com>
2020-03-08 17:51:13 +0800

    init repo

---
+++ src/test/java/com/ydsoft/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydsoft.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
